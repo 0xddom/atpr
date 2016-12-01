e8d44c1640aa7af196e1fe35287f4f6d7dc9230a
Aws0mus <alvaro.mepusieron@gmail.com>
2016-12-01 19:43:24 +0100

    Initial arg parser addded

--- ATPR/Program.cs
+++ ATPR/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
+using CommandLine;
+using CommandLine.Text;
 
 namespace ATPR
 {
 	class MainClass
 	{
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name="args">The command-line arguments.</param>
-		public static void Main(string[] args)
-		{
-			
+		// Define a class to receive parsed values
+		class Options {
+			[Option('p', "path", Required = true,
+				HelpText = "Directory where the tool will find the files.")]
+			public string InputFile { get; set; }
+
+			[Option('v', "verbose", DefaultValue = false,
+				HelpText = "Prints all messages to standard output.")]
+			public bool Verbose { get; set; }
+
+			[Option('o', "output", Required = false,
+				HelpText = "Output directory where the tool will save the results.")]
+			public bool Output { get; set; }
+				
+			[Option('c', "choose", Required = false,
+				HelpText = "Option of the tool.")]
+			public bool Choose { get; set; }
+
+			[HelpOption]
+			public string GetUsage() {
+				return HelpText.AutoBuild(this,
+					(HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+			}
+		}
+
+		// Consume them
+		static void Main(string[] args) {
+			var options = new Options();
+			if (CommandLine.Parser.Default.ParseArguments(args, options)) {
+				// Values are available here
+				if (options.Verbose) Console.WriteLine("Jiji jaja: {0}", options.InputFile);
+			}
 		}
 	}
 }
