2236beb72b2f8a2d9ae66523bf84a9466070b938
Aws0mus <alvaro.w.schuller@gmail.com>
2016-12-02 10:47:04 +0100

    Args parser done.

--- ATPR/Program.cs
+++ ATPR/Program.cs
@@ -11,7 +11,7 @@ namespace ATPR
 		/// </summary>
 		class Options {
 			[Option('p', "path", Required = true,
-				HelpText = "Directory where the tool will find the files.")]
+				HelpText = "File or directory where the tool will get the files.")]
 			public string InputFile { get; set; }
 
 			[Option('v', "verbose", DefaultValue = false,
@@ -19,7 +19,7 @@ namespace ATPR
 			public bool Verbose { get; set; }
 
 			[Option('o', "output", Required = false,
-				HelpText = "Output directory where the tool will save the results.")]
+				HelpText = "Output path where the tool will save the results.")]
 			public bool Output { get; set; }
 				
 			[Option('c', "choose", Required = true,
@@ -45,21 +45,36 @@ namespace ATPR
 			var options = new Options();
 			if (CommandLine.Parser.Default.ParseArguments(args, options)) {
 				// Values are available here
-				if (options.Verbose) Console.WriteLine("Running with options {0}", options.ToString());
+
+				if (options.Verbose) {
+					Console.Write("Running with options: ");
+					for (int i = 0; i < args.Length; i++)
+					{
+						Console.Write(" {0} ", args[i]);
+					}
+					Console.WriteLine("");
+				}
 				int choose = options.Choose;
 				switch (choose) {
 				case 1:	//Option 1, gets only entities
+					if (options.Verbose) Console.WriteLine("Option 1.");
 					break;
 				case 2:	//Option 2, generates dictionary
+					if (options.Verbose) Console.WriteLine("Option 2.");
 					break;
 				case 3:	//Option 3, gets entities that match with a dictionary
+					if (options.Verbose)
+						Console.WriteLine ("Option 3.");
+					if (options.Dictionary == null) {
+						Console.WriteLine("Dictionary required. Exiting...");
+						return;
+					}
+						
 					break;
 				default:
-					//ERROR
+					Console.WriteLine("Option not recognized. Exiting...");
 					break;
 				}
-
-
 			}
 		}
 	}
