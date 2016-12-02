5f5c326767f4654d4b9e8b7884119a551b994b9b
Daniel Domínguez <danieldominguez05@gmail.com>
2016-12-02 10:26:13 +0100

    Add boilerplater for code docs in Consts.cs

--- ATPRNER/Consts.cs
+++ ATPRNER/Consts.cs
@@ -3,8 +3,17 @@ namespace ATPRNER
 {
 	public class Consts
 	{
+		/// <summary>
+		/// The clasifiers.
+		/// </summary>
 		public static string CLASIFIERS = "/stanford-ner-2015-12-09/classifiers/";
-		public static string DEFAULT_STANFORD_NLP = "~/Hackaton/Stanford/s";
+		/// <summary>
+		/// The default stanford nlp.
+		/// </summary>
+		public static string DEFAULT_STANFORD_NLP = "~/Hackaton/Stanford/";
+		/// <summary>
+		/// The models.
+		/// </summary>
 		public static string MODELS = "/spanish.ancora.distsim.s512.crf.ser.gz";
 	}
 }
