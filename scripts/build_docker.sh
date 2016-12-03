db1724ce35af20112c8d038e02262bad5de332e7
Daniel Domínguez <danieldominguez05@gmail.com>
2016-12-03 10:31:45 +0100

    Add clean exit when not building docker

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,4 +3,6 @@
 IMAGE=kuroaku/atpr:latest
 APP_DIR=`pwd`
 
+[ "$BUILD_DOCKER" == 'no' ] && exit 0 # Exit cleanly in case this is not needed
+
 [ "$BUILD_DOCKER" == 'yes' ] && docker build -t $IMAGE $APP_DIR
